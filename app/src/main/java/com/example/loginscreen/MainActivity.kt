734dc1e2c77abaae20dda5ae3256145c4fef2d73
Erwin Kurniawan Adidharma <erwinkurniawanadi@gmail.com>
2020-09-29 21:00:42 +0700

    register ui

--- app/src/main/java/com/example/loginscreen/MainActivity.kt
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -9,10 +9,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    internal lateinit var rellay1: RelativeLayout
-    internal lateinit var rellay2: RelativeLayout
-    internal var handler = Handler()
-    internal var runnable: Runnable = Runnable {
+    private lateinit var rellay1: RelativeLayout
+    private lateinit var rellay2: RelativeLayout
+    private var handler = Handler()
+    private var runnable: Runnable = Runnable {
         rellay1.visibility = View.VISIBLE
         rellay2.visibility = View.VISIBLE
     }
